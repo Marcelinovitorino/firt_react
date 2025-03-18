757b09bb6638a148d9e79578387c74c59fc260a5
Marcelinovitorino <168681717+Marcelinovitorino@users.noreply.github.com>
2025-03-17 23:47:41 -0800

    mdd

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './style.css'
 
-function App() {
+function Home() {
   const [count, setCount] = useState(0)
 
   return (
@@ -16,4 +14,4 @@ function App() {
   )
 }
 
-export default App
+export default Home
