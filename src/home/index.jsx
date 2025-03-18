3deda8f902503955afa9a31a6c96235a30b8ff80
Marcelinovitorino <168681717+Marcelinovitorino@users.noreply.github.com>
2025-03-18 00:03:56 -0800

    sdsk

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -6,9 +6,24 @@ function Home() {
 
   return (
 
-    <div>
-      <h1>Hello world!</h1>
-      
+    <div className='container'>
+      <form >
+        <h1>Cadastro de Usuarios</h1>
+        <p>
+          <input type="text" name="name" placeholder='insira o seu nome' />
+
+        </p>
+        <p>
+          <input type="text" name="email" placeholder='insira o seu email' />
+        </p>
+        <p>
+          <input type="text" name="password" placeholder='insira o sua password' />
+        </p>
+        <button type="button">Cadastrar</button>
+
+      </form>
+
+
 
     </div>
   )
